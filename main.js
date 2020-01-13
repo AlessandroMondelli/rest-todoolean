f48cc604f4cebb2411686c5a5a63423466650fb2
Alessandro_Mondelli <alexs98@hotmail.it>
2020-01-13 23:14:30 +0100

    Aggiunta modifica

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    var urlToDo = 'http://157.230.17.132:3014/todos/'; //Url API
 
     //Preparo template Handlebars
     var source = $("#to-do-template").html();
@@ -22,16 +23,33 @@ $(document).ready(function() {
         hideInput(); //Richiamo funzione per nascondere input
     });
 
-    $(document).on('click', '.to-do-el i', function() { //Se cliccato X su lista..
+    $(document).on('click', '.to-do-el .fa-times', function() { //Se cliccato X su lista..
         var id = $(this).parent().attr('data-to-do'); //Prendo id elemento
         removeData(id); //Richiamo funzione per rimuovere elemento
     });
 
+    $(document).on('click', '.fa-pencil-alt', function() { //Se cliccata la matita..
+        var parentToDo = $(this).parent(); //Padre della matita
+
+        showMod(parentToDo); //Richiamo funzione che mostra modifica
+    });
+
+    $(document).on('click', '.fa-save', function() { //Se cliccato il floppy
+        var parentToDo = $(this).parent(); //Padre della matita
+
+        var parentToDoId = parentToDo.attr('data-to-do'); //Prendo data id
+        var getText = parentToDo.find(".mod-to-do").val(); //Prendo testo modifica
+        if (getText.length > 0) { //Se l'input non è vuoto
+            modData(parentToDoId,getText); //Richiamo funzione per modificare
+        }
+    });
+
+
     //*** FUNZIONI ***/
 
     function printToDoData() {
-        $.ajax ({ //Chiamata AJAX per recuperare dati film
-            'url' : 'http://157.230.17.132:3014/todos',
+        $.ajax ({ //Chiamata AJAX per recuperare dati
+            'url' : urlToDo,
             'method' : 'GET', //Metodo GET
             'success' : function(data) { //Caso funzionamento richiesta
                 $("#to-do-list").empty(); //Svuoto lista precedente
@@ -51,8 +69,8 @@ $(document).ready(function() {
     }
 
     function addToData(addToDo) { //Funzione che aggiunge elemento
-        $.ajax ({ //Chiamata AJAX per recuperare dati film
-            'url' : 'http://157.230.17.132:3014/todos',
+        $.ajax ({ //Chiamata AJAX per recuperare dati
+            'url' : urlToDo,
             'method' : 'POST', //Metodo POST
             'data' : {
                 'text' : addToDo
@@ -67,9 +85,25 @@ $(document).ready(function() {
     }
 
     function removeData(idToDo) { //Funzione che rimuove elemento
-        $.ajax ({ //Chiamata AJAX per recuperare dati film
-            'url' : 'http://157.230.17.132:3014/todos/' + idToDo,
+        $.ajax ({ //Chiamata AJAX per recuperare dati
+            'url' : urlToDo + idToDo,
             'method' : 'DELETE', //Metodo DELETE
+            'success' : function(data) { //Caso funzionamento richiesta
+                printToDoData(); //Richiamo funzione per stampare per aggiornare lista
+            },
+            'error' : function() { //Caso di errore di caricamento
+                alert("Errore");
+            }
+        });
+    }
+
+    function modData(idSel,newText) { //Funzione che modifica elemento
+        $.ajax ({ //Chiamata AJAX per recuperare dati
+            'url' : urlToDo + idSel,
+            'method' : 'PUT', //Metodo PUT
+            'data' : {
+                'text' : newText //Nuovo testo
+            },
             'success' : function(data) { //Caso funzionamento richiesta
                 printToDoData(); //Richiamo funzione per stampare per aggiornare lista
             },
@@ -84,5 +118,12 @@ $(document).ready(function() {
         $("#textbox-to-do").removeClass("active"); //Nascondo input
         $("body").removeClass("not-active"); //Nascondo colore scuro body
         $("#abort").removeClass("active"); //Mostro pulsante per annullare
+    }
+
+    function showMod(parent) {  //Funzione che mostra modifica
+        parent.find(".mod-to-do").addClass("active"); //Mostro input
+        parent.find(".to-do-text").addClass("hidden"); //Nascondo testo
+        parent.find(".fa-save").addClass("on"); //Mostro Floppy
+        parent.find(".fa-pencil-alt").hide(); //Nascondo penna
     }
 });
